4e449166687896c7fbb0bf36043890f81ceae228
Sovidi <lim132445@gmail.com>
2024-01-22 22:15:00 +0900

    sw

--- src/component/Main.js
+++ src/component/Main.js
@@ -13,6 +13,7 @@ function Main() {
     if(!sec[0] || !sec[1] || !sec[2] || !sec[3]) return <>로딩중</>;
     // if(sec[0] == undefined || sec[1] == undefined || sec[2] == undefined || sec[3] == undefined) return <>로딩중</>;
     return (
+			<>
         <Swiper
 					spaceBetween={30}
 					slidesPerView={3}
@@ -21,8 +22,7 @@ function Main() {
 					onSlideChange={() => console.log('slide change')}
 					onSwiper={(swiper) => console.log(swiper)}
 				>
-						<div className='swiper-wrapper'>
-						{
+            {
                 sec[0].data.results.map(item => (
                     <SwiperSlide>
                         <code>{item.original_title}{item.original_name}</code>
@@ -31,41 +31,65 @@ function Main() {
                     </SwiperSlide>
                 ))
             }
-						</div>
-						<div className='swiper-wrapper'>
-						{
-                sec[1].data.results.map(item => (
-                    <li>
-                        <code>{item.original_title}{item.original_name}</code>
-                        {/* <a onClick={()=>{navigate("/pop", {state:{item}})}}></a> */}
-                        <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}></img>
-                    </li>
-                ))
-            }
-						</div>
-						<div className='swiper-wrapper'>
-						{
-                sec[2].data.results.map(item => (
-                    <li>
-                        <code>{item.original_title}{item.original_name}</code>
-                        {/* <a onClick={()=>{navigate("/pop", {state:{item}})}}></a> */}
-                        <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}></img>
-                    </li>
-                ))
-            }
-						</div>
-						<div className='swiper-wrapper'>
-						{
-                sec[3].data.results.map(item => (
-                    <li>
-                        <code>{item.original_title}{item.original_name}</code>
-                        {/* <a onClick={()=>{navigate("/pop", {state:{item}})}}></a> */}
-                        <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}></img>
-                    </li>
-                ))
-            }
-						</div>
 					</Swiper>
+					<Swiper
+										spaceBetween={30}
+										slidesPerView={3}
+										navigation={true}
+										modules={[Navigation]}
+										onSlideChange={() => console.log('slide change')}
+										onSwiper={(swiper) => console.log(swiper)}
+					>
+
+            {
+                sec[1].data.results.map(item => (
+                    <SwiperSlide>
+                        <code>{item.original_title}{item.original_name}</code>
+                        {/* <a onClick={()=>{navigate("/pop", {state:{item}})}}></a> */}
+                        <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}></img>
+                    </SwiperSlide>
+                ))
+            }
+						</Swiper>
+
+						<Swiper
+											spaceBetween={30}
+											slidesPerView={3}
+											navigation={true}
+											modules={[Navigation]}
+											onSlideChange={() => console.log('slide change')}
+											onSwiper={(swiper) => console.log(swiper)}
+						>
+            {
+                sec[2].data.results.map(item => (
+                    <SwiperSlide>
+                        <code>{item.original_title}{item.original_name}</code>
+                        {/* <a onClick={()=>{navigate("/pop", {state:{item}})}}></a> */}
+                        <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}></img>
+                    </SwiperSlide>
+                ))
+            }
+						</Swiper>
+
+						<Swiper
+											spaceBetween={30}
+											slidesPerView={3}
+											navigation={true}
+											modules={[Navigation]}
+											onSlideChange={() => console.log('slide change')}
+											onSwiper={(swiper) => console.log(swiper)}
+						>
+            {
+                sec[3].data.results.map(item => (
+                    <SwiperSlide>
+                        <code>{item.original_title}{item.original_name}</code>
+                        {/* <a onClick={()=>{navigate("/pop", {state:{item}})}}></a> */}
+                        <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}></img>
+                    </SwiperSlide>
+                ))
+            }
+        		</Swiper>
+						</>
     )
 }
 
