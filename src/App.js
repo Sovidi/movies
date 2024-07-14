c93b5a68d236a5a5ff2ab3477fdace4e67c42203
Sovidi <lim132445@gmail.com>
2024-07-14 17:25:01 +0900

    hsbn

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Search from './component/Search';
 function App() {
   return (
     <Context>
-      <BrowserRouter>
+      <BrowserRouter basename='movies'>
         <Header/>
         <main>
           <Routes>
